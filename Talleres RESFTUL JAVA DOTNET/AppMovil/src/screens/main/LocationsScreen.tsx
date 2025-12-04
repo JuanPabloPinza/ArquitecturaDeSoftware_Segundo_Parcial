8b85a879e8988fa7d9eadeb1ac82b5ace6416a91
Joel Daniel Arguello Espinosa <joel.darguello@gmail.com>
2025-12-03 19:39:19 -0500

    feat: Agrega la clave API de Google Maps en LocationsScreen

--- Talleres RESFTUL JAVA DOTNET/AppMovil/src/screens/main/LocationsScreen.tsx
+++ Talleres RESFTUL JAVA DOTNET/AppMovil/src/screens/main/LocationsScreen.tsx
@@ -21,7 +21,7 @@ import {
   WebMap,
 } from "./locations";
 
-const API_KEY = "";
+const API_KEY = "AQUI_API_KEY_GOOGLE_MAPS";
 
 export const LocationsScreen: React.FC = () => {
   const insets = useSafeAreaInsets();
